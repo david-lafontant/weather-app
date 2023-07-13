0c5b17dcf3ad428973c5ce5ab8f86be29317f7ff
David Lafontant <lafontant.david@gmail.com>
2023-07-13 15:06:03 -0400

    Display data and manage error

--- app.js
+++ app.js
@@ -7,9 +7,27 @@ const longitude = 0.1276
 const url = `http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${latitude},${longitude}&aqi=no`
 
 request(url, function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log(chalk.blue('body:', body)); // Print the HTML for the Google homepage.
-});
 
+  try{
+    console.log('statusCode:', response && response.statusCode); 
+    const data = JSON.parse(body)
+    console.log("location: ",chalk.blue(data.location.name))
+    console.log("current temperature: ",chalk.blue(data.current.temp_c,"C"))
+    console.log("current condition: ",chalk.blue(data.current.condition.text))
+}catch(error){
+  console.log(chalk.red.inverse("Invalid request"))
+  console.log(chalk.red('Error:', error.message));
+}
+
+  if(error){
+    console.error('error:', error.message); 
+  }
+else if (response.body.error) {
+  console.log('Error:', response.body.error);
+} else{
+
+
+}
+
+});
 
